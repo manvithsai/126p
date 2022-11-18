c1190236defc1aff0a0477a0936de0b7ae3c2065
manvithsai <90837315+manvithsai@users.noreply.github.com>
2022-11-18 18:58:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup(){
+canvas=createCanvas(500,500);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+function draw(){
+image(video,0,0,500,500);
+}
+song="";
+song2="";
+function preload(){
+song=loadSound("music.mp3");
+song2=loadSound("music2.mp3");
+}
